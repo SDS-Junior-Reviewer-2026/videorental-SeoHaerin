bcd1e1b19e52815fc2cb79b406c63141eaf6f472
west-sea <seika3695@gmail.com>
2026-04-01 16:20:36 +0900

    git testcase 100 cover

--- src/test/java/com/videorental/CustomerTest.java
+++ src/test/java/com/videorental/CustomerTest.java
@@ -4,6 +4,82 @@ import org.junit.jupiter.api.Test;
 
 public class CustomerTest {
     @Test
+    void statementForNoRental() {
+        Customer customer= new Customer("NAME_NOT_IMPORTANT");
+        String statement= customer.statement();
+        Assertions.assertEquals("Rental Record for NAME_NOT_IMPORTANT\n"
+                + "Amount owed is 0.0\n"
+                + "You earned 0 frequent renter pointers", statement);
+    }
+    @Test
+    void statementForRegularMovieRentalForLessThan3Days() {
+        Customer customer= new Customer("NAME_NOT_IMPORTANT");
+        customer.addRental(new Rental(new Movie("TITAN", Movie.REGULAR),
+                2));
+        String statement= customer.statement();
+        Assertions.assertEquals("Rental Record for NAME_NOT_IMPORTANT\n"
+                + "\t2.0(TITAN)\n"
+                + "Amount owed is 2.0\n"
+                + "You earned 1 frequent renter pointers", statement);
+    }
+    @Test
+    void statementForRegularMovieRentalForMoreThan2Days() {
+        Customer customer= new Customer("NAME_NOT_IMPORTANT");
+        customer.addRental(new Rental(new Movie("TITAN", Movie.REGULAR),
+                3));
+        String statement= customer.statement();
+        Assertions.assertEquals("Rental Record for NAME_NOT_IMPORTANT\n"
+                + "\t3.5(TITAN)\n"
+                + "Amount owed is 3.5\n"
+                + "You earned 1 frequent renter pointers", statement);
+    }
+    @Test
+    void statementForNewReleaseMovie() {
+        Customer customer= new Customer("NAME_NOT_IMPORTANT");
+        customer.addRental(new Rental(new Movie("MERRY", Movie.NEW_RELEASE),
+                1));
+        String statement= customer.statement();
+        Assertions.assertEquals("Rental Record for NAME_NOT_IMPORTANT\n"
+                + "\t3.0(MERRY)\n"
+                + "Amount owed is 3.0\n"
+                + "You earned 1 frequent renter pointers", statement);
+    }
+    @Test
+    void statementForChildrensMovieRentalMoreThan3Days() {
+        Customer customer= new Customer("NAME_NOT_IMPORTANT");
+        customer.addRental(new Rental(new Movie("UP", Movie.CHILDRENS),
+                4));
+        String statement= customer.statement();
+        Assertions.assertEquals("Rental Record for NAME_NOT_IMPORTANT\n"
+                + "\t3.0(UP)\n"
+                + "Amount owed is 3.0\n"
+                + "You earned 1 frequent renter pointers", statement);
+    }
+    @Test
+    void statementForChildrensMovieRentalLessThan4Days() {
+        Customer customer= new Customer("NAME_NOT_IMPORTANT");
+        customer.addRental(new Rental(new Movie("DROWNING", Movie.CHILDRENS),
+                3));
+        String statement= customer.statement();
+        Assertions.assertEquals("Rental Record for NAME_NOT_IMPORTANT\n"
+                + "\t1.5(DROWNING)\n"
+                + "Amount owed is 1.5\n"
+                + "You earned 1 frequent renter pointers", statement);
+    }
+    @Test
+    void statementForNewReleaseMovieRentalMoreThan1Day() {
+        Customer customer= new Customer("NAME_NOT_IMPORTANT");
+        customer.addRental(new Rental(new Movie("KING LIVE MAN", Movie.NEW_RELEASE),
+                2));
+        String statement= customer.statement();
+        Assertions.assertEquals("Rental Record for NAME_NOT_IMPORTANT\n"
+                + "\t6.0(KING LIVE MAN)\n"
+                + "Amount owed is 6.0\n"
+                + "You earned 2 frequent renter pointers", statement);
+    }
+
+
+    @Test
     void sample() {
         Customer customer = new Customer("Bob");
         customer.addRental(new Rental(new Movie("Jaws", Movie.REGULAR),
@@ -31,4 +107,6 @@ public class CustomerTest {
                 "Amount owed is 19.0\n" +
                 "You earned 7 frequent renter pointers", customer.statement());
     }
+
+
 }
